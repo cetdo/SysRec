aae4cf9339025566506b67c0820ac8a6a0a8f1d1
cetdo <ceto.snf17@uea.edu.br>
2019-10-02 22:55:28 -0400

    Versao agora com o calculo da similaridade

--- Project/sysRec.py
+++ Project/sysRec.py
@@ -1,4 +1,5 @@
-import csv, math
+import csv
+import math
 
 
 class User(object):  # cria a classe usuário
@@ -13,14 +14,17 @@ class User(object):  # cria a classe usuário
     def print(self):  # imprime os dados do usuário
         print(f'ID: {self.id} Nome: {self.name} Avaliações: {self.ratings}')
 
+
 class Similarity(object):
-    def __init__(self, id1, id2, sim):
+    def __init__(self, id1, id2, category, sim):
         self.id1 = id1
         self.id2 = id2
+        self.category = category
         self.sim = sim
 
     def getSim(self):
-        return self.sim
+        return (f'Usuarios {self.id1} e {self.id2}, Tipo: {self.category}, Taxa de Similaridade: {self.sim}')
+
 
 def getUsers(fileName):  # função para recurepar os dados do .csv e colocar num array da classe usuario
     users = []
@@ -34,12 +38,13 @@ def getUsers(fileName):  # função para recurepar os dados do .csv e colocar nu
             # pega todas as classificações (colunas de 3 a 12) e coloca num vetor
             for i in range(2, 12):
                 # adiciona as classificações em ordem num vetor
-                a.append(lines[line][i])
+                a.append(int(lines[line][i]))
 
             # instancia e adiciona no vetor um objeto da classe usuario
             users.append(User(lines[line][0], lines[line][1], a))
 
     return users  # retorna um vetor de usuários instanciados e suas avalizações
+
 
 
 def getPearson(user1, user2):
@@ -58,26 +63,60 @@ def getPearson(user1, user2):
         sumXY += x[i] * y[i]
         sumX += x[i]
         sumY += y[i]
-        sumX2 += math.pow(x[i],2)
-        sumY2 += math.pow(y[i],2)
+        sumX2 += math.pow(x[i], 2)
+        sumY2 += math.pow(y[i], 2)
 
     over = sumXY - ((sumX*sumY)/n)
 
-    under1 = math.sqrt(sumX2-(math.pow(sumX,2)/n))
-    under2 = math.sqrt(sumY2-(math.pow(sumY,2)/n))
+    under1 = math.sqrt(sumX2-(math.pow(sumX, 2)/n))
+    under2 = math.sqrt(sumY2-(math.pow(sumY, 2)/n))
 
     under = under1 * under2
 
     return (over/under)
 
+
+
+def getCosine(user1, user2):
+    sumXY = 0
+    sumX2 = 0
+    sumY2 = 0
+    x = user1.getRatings()
+    y = user2.getRatings()
+
+    for i in range(len(x)):
+        sumXY += x[i]*y[i]
+        sumX2 += math.pow(x[i],2)
+        sumY2 += math.pow(y[i],2)
+
+    ret = math.sqrt(sumX2)*math.sqrt(sumY2)
+
+    if (ret != 0):
+        ret = (sumXY/(math.sqrt(sumX2)*math.sqrt(sumY2)))
+    else:
+        ret = -1
+
+
+
+    return ret
+
 def debug():
-    #users = getUsers('users.csv')
-    #for user in users:
-    #    user.print()
+    users = getUsers('users.csv')
+    sims = []
+    user = users[0]
+    for i in range(1,len(users)):
+        simi = getCosine(user, users[i])
+        sims.append(Similarity('1',users[i].id,'Cosseno',simi))
 
-    user3 = User('01','Cadu', [4.75,4.5,5,4.25,4])
-    user4 = User('02','Gabe', [4,3,5,2,1])
+    for s in sims:
+        print(s.getSim())
 
-    print(getPearson(user3,user4))
+    #user3 = User('01', 'Cadu', [4.75, 4.5, 5, 4.25, 4])
+    #user4 = User('02', 'Gabe', [4, 3, 5, 2, 1])
+    #sim = Similarity(user3.id, user4.id, 'Cossenos', getCosine(user3, user4))
+
+    #print(sim.getSim())
+
 
 debug()
+
